445bec5ff8447fc32033d564b436055d4a310a58
Dennis Timlin <Dennis.timlin@ars.usda.gov>
2015-02-03 16:23:19 -0500

    doxygen codes were modified

--- Photosynthesis(notManaged).cpp
+++ Photosynthesis(notManaged).cpp
@@ -1,11 +1,18 @@
-// Photosynthesis(notManaged).cpp : Defines the entry point for the console application.
-// This program demonstrates how to call the photosynthesis module
-// it reads two files
-//   1. a parameter file for a particular plant species
-//   2. an environmental file with temperature, radiation, CO2 content, humidity, wind
-//       and a flag (0,1) to tell the program if constant temperature is used (or let temperature of leaf
-//       vary with stomatal conductance. 
-//
+/*! \class _tmain 
+* interface to photosynthesis module
+* \par
+*  This program demonstrates how to call the photosynthesis module
+*  Photosynthesis(notManaged).cpp : Defines the entry point for the console application.
+*
+* \par Use of this interface
+* Two files are needed:
+*    1. a parameter file containing the parameters for the photosynthesis module, one line for each species
+*    2. an environmental file with temperature, radiation, CO2 content, humidity, wind
+*       and a flag (0,1) to tell the program if constant temperature is used (or let temperature of leaf
+*       vary with stomatal conductance. 
+*
+*/
+  
 
 #include "stdafx.h"
 #include "gas_exchange.h"
@@ -15,43 +22,50 @@
 #include <sstream>
 
 using namespace std;
-// using std::string is a more generic method
-using namespace photomod;
+// uses std::string, a more generic method than CString
+using namespace photomod; //Photosynthesis module
 
-
-int _tmain(int argc, _TCHAR* argv[])
+//* @author Dennis Timlin
+int _tmain(int argc, _TCHAR* argv[]) 
 {    
-	string DataLine; //!< holds line of data read from file
-	string Species ="maize"; //!< Species of plant for calculatioins
-	string Remark; //!< from parameter file
-	char* context = NULL; //!< needed to use strtok_s functin
+	string DataLine;         //!< holds line of data read from file
+	string Remark;           //!< from parameter file
+	char* context = NULL;    //!< needed to use strtok_s function
 	double PFD, Temperature, RelativeHumidity,Wind, CO2, Pressure=100;
 	bool ConstantTemperature; //!< parameters sent to photosynthesis module
 	// photomod::CGasExchange::tParms *thisParms;
 	photomod::CGasExchange::tParms thisParms; //!< declare a photosynthesis object
-	const char *token=","; //!< pointer to character that separates entries in the parameter file
-	char * pnt; //!< pointer to the next word to be read from parameter file
-	bool test; 
-	char CharTest ='A'; //!< variable to test if there are characters in the line of data (indicates end of data)
-	bool found=false; //!< Boolean to indicate the species line was found in the parameter file
-	//char *dup; // need to copy string objects to a char* in order to use strtok_s()
-	string temp ; //variable for holding string objects
+	const char *delim=",";    //!< pointer to character (delimiter) that separates entries in the parameter file
+	char * pnt;               //!< pointer to the next word to be read from parameter file
+	char CharTest ='A';       //!< variable to test if there are characters in the line of data (indicates end of data)
+	bool found=false;         //!< Boolean to indicate the species line was found in the parameter file
+	//char *dup;              // need to copy string objects to a char* in order to use strtok_s()
+	string temp ;             //!< temporary variable for holding string objects
 
 	ifstream ParamFile, DataFile;  //!< files to hold parameters and variables for a single run
 	ofstream OutputFile;            //!<holds data output from photosynthesis module
-	string DataFileName="ClimateIn.dat", OutputFileName="Results.dat";
-	// Get datafile name, and species name
+
+	//variables to hold results from gas exchange module.
+	double Anet, VPD,Agross,LeafTemperature, Respiration, InternalCO2, StomatalConductance,
+		BoundaryLayerConductance;
+
+
+//assign defaults in case user does not want to enter other information (DataLine is empty)
+	string DataFileName="ClimateIn.dat", OutputFileName="Results.dat"; 
+	string Species ="maize"; //!< Species of plant for calculatioins
+
+	// Get datafile name, and species name if entered by user.
 	cout << "enter name of file with input data and species name separated by commas:" <<endl
 		<< "hit Enter with empty string for defaults" <<endl;
 	getline(cin,DataLine);
-	if (!DataLine.empty())
+	if (!DataLine.empty()) //if empty defaults to string names assigned above
 	{
 
-		pnt=strtok_s((char*)DataLine.c_str(), token, &context );  //pnt is a pointer to the last recently found token in the string
-		DataFileName.assign(pnt);
-		pnt=strtok_s(NULL, token, &context );  //pnt is a pointer to the last recently found token in the string
-		Species.assign(pnt);
-		Species.erase(remove(Species.begin(),Species.end(),' '),Species.end());
+		pnt=strtok_s((char*)DataLine.c_str(), delim, &context );  //pnt is a pointer to the last recently found token in the string
+		DataFileName.assign(pnt);                                // first token is a file name
+		pnt=strtok_s(NULL, delim, &context );                    //get ready for the next token by getting pointer to recently found token in the string
+		Species.assign(pnt);                                     // next token is species
+		Species.erase(remove(Species.begin(),Species.end(),' '),Species.end()); //remove blanks from species name in case any are present
 		pnt=NULL; //finished with these two
 	}
 	// open file with parameters and data
@@ -59,16 +73,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	DataFile.open(DataFileName.c_str());
 	FILE * pFile;
 	pFile=fopen((char*)OutputFileName.c_str(),"w");
-	fprintf(pFile, "ANet     AGross    VPD  Leaf Temp   BoundaryL conduc  InternalCO2  Respiration  StomatalConduct \n");
+	fprintf(pFile, "ANet     AGross    VPD  Leaf Temp   BoundaryL_Conduc  InternalCO2  Respiration  StomatalConduct \n");
 	OutputFile.open(OutputFileName.c_str());
 	std::getline(ParamFile,DataLine); //get header from parameter file
     // last line of file may be empty or contain numbers, this indicates file is at the  end
-	while (!ParamFile.eof() && isalpha(CharTest))
+	while (!ParamFile.eof() && isalpha(CharTest))  //loops through file reads each line and tests if the correct species is found
 	{
-		int test=DataLine.size();
 		getline(ParamFile,DataLine); // get the first line of data 
 		CharTest=DataLine.at(0);     //check that line contains alphabetical text at beginning
-		pnt=strtok_s((char*)DataLine.c_str(), token, &context ); // pick off the first word before the token (',')
+		pnt=strtok_s((char*)DataLine.c_str(), delim, &context ); // pick off the first word before the token (',')
 
 		//First read file to find the desired plant species
 		temp.assign(pnt);
@@ -82,84 +95,83 @@ int _tmain(int argc, _TCHAR* argv[])
 		transform(thisParms.ID.begin(), thisParms.ID.end(),thisParms.ID.begin(), ::tolower);
 		transform(Species.begin(), Species.end(),Species.begin(), ::tolower);
 
-		//Search for the correct species
-		if (thisParms.ID.compare(Species)==0 && !found)  //continue to parse string
+		//Search for the correct species in file
+		if (thisParms.ID.compare(Species)==0 && !found)  //continue to parse string 
 		{
 
-			pnt = strtok_s( NULL,token, &context ); // iterate to clean string of characters already read
+			pnt = strtok_s( NULL,delim, &context ); // iterate to clean string of characters already read
 			// This section parses string, each iteration it cleans string of characters already read
 			while(pnt!=NULL ) 
 			{
-				// printf( "Tokenized string using *  is:: %s\n", pnt );
+				// printf( "Tokenized string using *  is:: %s\n", pnt ); // for debugging
 
 				temp.assign(pnt);
 				thisParms.species=temp;
 				temp.clear();
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				temp.assign(pnt);
 				thisParms.Type=temp;
 				temp.clear();
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Vcm25=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Jm25=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Vpm25=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.TPU25=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Rd25=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Theta=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.EaVc=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Eaj=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Hj=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Sj=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Hv=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Sv=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Eap=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.Ear=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.g0=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.g1=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.stomaRatio=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.LfWidth=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				thisParms.LfAngFact=atof(pnt);
-				pnt = strtok_s( NULL,token, &context ); 
+				pnt = strtok_s( NULL,delim, &context ); 
 				temp.assign(pnt);
 				thisParms.Remark=temp;
 				temp.clear();
-				pnt=strtok_s(NULL,token, &context);
+				pnt=strtok_s(NULL,delim, &context);
 				found=true;
 			}
 
 		}
 	}
 	// Implementation to interact with photosynthesis model is here
-	CharTest='1';
+	CharTest='1'; //initialize CharTest
 	int start=1;
-	bool LineEmpty=false;
+	bool LineEmpty=false; //Checks if file with environmental data is finished.
 	//output variables
-	double Anet, VPD,Agross,LeafTemperature, Respiration, InternalCO2, StomatalConductance,
-		BoundaryLayerConductance;
+	//Define a pointer to a new variable as a GasExchange Object
+	photomod::CGasExchange *MyLeafGasEx;
 	// Create a new GasExchange Object
-	photomod::CGasExchange *MyLeafGas;
-	MyLeafGas= new CGasExchange();
-	//Pass the plant species paramters read before
-	MyLeafGas->SetParams(&thisParms);
-	// loop to read environmental input data and calculate output
+	MyLeafGasEx= new CGasExchange();
+	//Initialize Gas exchange object by passing species and relavent parameters read earlier
+	MyLeafGasEx->SetParams(&thisParms);
+	// loop to read environmental input data and call object to calculate results
 	while (!LineEmpty)
 		
 	{
@@ -169,30 +181,31 @@ int _tmain(int argc, _TCHAR* argv[])
 	else
 	{
 	
-		CharTest=DataLine.at(0);
-		pnt=strtok_s((char*)DataLine.c_str(), token, &context ); 
+		//CharTest=DataLine.at(0); // check for valid data
+		pnt=strtok_s((char*)DataLine.c_str(), delim, &context );  //token is the delimiter
 		PFD=atof(pnt);
-		pnt=strtok_s(NULL,token,&context);
+		pnt=strtok_s(NULL,delim,&context);
 		Temperature=atof(pnt);
-		pnt=strtok_s(NULL,token,&context);
+		pnt=strtok_s(NULL,delim,&context);
 		CO2=atof(pnt);
-		pnt=strtok_s(NULL,token,&context);
+		pnt=strtok_s(NULL,delim,&context);
 		RelativeHumidity=atof(pnt);
-		pnt=strtok_s(NULL,token,&context);
+		pnt=strtok_s(NULL,delim,&context);
 		Wind=atof(pnt);
-		pnt=strtok_s(NULL,token,&context);
+		pnt=strtok_s(NULL,delim,&context);
 		ConstantTemperature=atoi(pnt);
-		MyLeafGas->SetVal(PFD, Temperature, CO2, RelativeHumidity, 
+		// pass relavent environmental variables to gas exchange object and execute module
+		MyLeafGasEx->SetVal(PFD, Temperature, CO2, RelativeHumidity, 
 			Wind, Pressure, ConstantTemperature); 
-		//MyLeafGas =new photomod::CGasExchange;
-		Anet=MyLeafGas->get_ANet();
-		Agross=MyLeafGas->get_AGross();
-		VPD=MyLeafGas->get_VPD();
-		LeafTemperature=MyLeafGas->get_LeafTemperature();
-		BoundaryLayerConductance=MyLeafGas->get_BoundaryLayerConductance();
-		InternalCO2=MyLeafGas->get_Ci();
-		Respiration=MyLeafGas->get_Respiration();
-		StomatalConductance=MyLeafGas->get_StomatalConductance();
+		// Return calculated variables from gas exchange object
+		Anet=MyLeafGasEx->get_ANet();
+		Agross=MyLeafGasEx->get_AGross();
+		VPD=MyLeafGasEx->get_VPD();
+		LeafTemperature=MyLeafGasEx->get_LeafTemperature();
+		BoundaryLayerConductance=MyLeafGasEx->get_BoundaryLayerConductance();
+		InternalCO2=MyLeafGasEx->get_Ci();
+		Respiration=MyLeafGasEx->get_Respiration();
+		StomatalConductance=MyLeafGasEx->get_StomatalConductance();
 
 
 
